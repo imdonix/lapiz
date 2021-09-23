69ef83c829833e22ab6e40b4fb8746247d0bea91
Magyar Tamás <tamas.donix@gmail.com>
2021-09-23 23:17:42 +0200

    Entitytag scrach added

---
+++ Assets/Code/HUD/Entities/EntityTag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityTag : MonoBehaviour
+{
+
+    public void Bind(Entity entity)
+    { 
+    
+    }
+
+}
